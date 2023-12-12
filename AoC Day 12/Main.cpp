19b47ce42ddd6937ae0b6c2dbec3c02bd9e3c7f2
andrei-ned <nedelcuandrei64@gmail.com>
2023-12-13 01:15:40 +0200

    day 12 part 2

--- AoC Day 12/Main.cpp
+++ AoC Day 12/Main.cpp
@@ -8,76 +8,49 @@
 
 using namespace std;
 
-int getBit(int target, int bitIdx)
+unordered_map<string, uint64_t> cache;
+uint64_t countCache(string springs, vector<int> groups)
 {
-	return (target >> bitIdx) & 1;
-}
-
-bool matchesGroups(string spring, vector<int> groups)
-{
-	int idx = 0;
-	int count = 0;
-	char last = '-';
-	for (int i = 0; i < spring.length(); i++)
+	string key = springs;
+	for (int i = 0; i < groups.size(); i++)
 	{
-		if (spring[i] == '#')
-		{
-			count++;
-		}
-		else
-		{
-			if (count == 0)
-				continue;
-			if (idx == groups.size())
-				return false;
-			if (groups[idx] != count)
-				return false;
-			idx++;
-			count = 0;
-		}
+		key += groups[i];
 	}
-	if (count > 0)
+	if (cache.contains(key))
 	{
-		if (idx == groups.size())
-			return false;
-		if (groups[idx] != count)
-			return false;
-		idx++;
+		return cache[key];
 	}
-	return idx == groups.size();
-}
-
-int countArrangements(string springs, vector<int>& groups)
-{
-	//cout << "Checking " << springs << "\n";
-
-	int matches = 0;
-	int countQuestions = 0;
-	for (int i = 0; i < springs.length(); i++)
-		if (springs[i] == '?')
-			countQuestions++;
-	for (int i = 0; i < 1 << countQuestions; i++)
+	if (groups.size() == 0)
 	{
-		string copySpring = springs;
-		int qIdx = 0;
-		for (int j = 0; j < copySpring.length(); j++)
-		{
-			if (copySpring[j] == '?')
-			{
-				copySpring[j] = getBit(i, qIdx) ? '#' : '.';
-				qIdx++;
-			}
-		}
-		if (matchesGroups(copySpring, groups))
-		{
-			//cout << "Found    " << copySpring << "\n";
-			matches++;
-		}
+		return springs.find('#') == string::npos ? 1 : 0; // if there is still # then we don't have a group for it
 	}
-
-	//cout << "\n";
-	cout << "Total " << matches << " matches for " << springs << "\n";
-	return matches;
+	if (springs.length() == 0)
+	{
+		return 0; // there's still groups, but we ran out of characters
+	}
+	uint64_t count = 0;
+	char first = springs[0];
+	if (first == '.' || first == '?')
+	{
+		string copy = springs;
+		copy.erase(0, 1);
+		count += countCache(copy, groups); // recurse without first character
+	}
+	if (first == '#' || first == '?') // check if this can be a group
+	{
+		if (springs.length() < groups[0]) // not enough characters for this group
+			return count;
+		for (int i = 0; i < groups[0]; i++) // check group not to have '.'
+			if (springs[i] == '.') // don't have room for a group of this length
+				return count;
+		if (springs[groups[0]] == '#') // element after group shouldn't be part of group
+			return count;
+		springs.erase(0, groups[0] + 1); // delete group + element after the group
+		groups.erase(groups.begin());
+		count += countCache(springs, groups); // recurse without first group and its characters
+	}
+	cache[key] = count;
+	return count;
 }
 
 int main()
@@ -86,7 +59,9 @@ int main()
 
 	string currentIn;
 
-	int sumArrangements = 0;
+	uint64_t sumArrangements = 0;
+	uint64_t sumUnfolded = 0;
+
 	while (fin >> currentIn)
 	{
 		string groupSizes;
@@ -100,10 +75,23 @@ int main()
 			sin >> comma;
 			groups.push_back(currSize);
 		}
-		sumArrangements += countArrangements(currentIn, groups);
-		//cout << currentIn << ": " << matchesGroups(currentIn, groups) << "\n";
+
+		// part 2, unfold records
+		string unfolded = currentIn;
+		vector<int> unfoldedGroups;
+		for (int i = 0; i < 4; i++)
+			unfolded += "?" + currentIn;
+		for (int i = 0; i < 5; i++)
+		{
+			for (int j = 0; j < groups.size(); j++)
+				unfoldedGroups.push_back(groups[j]);
+		}
+
+		sumArrangements += countCache(currentIn, groups);
+		sumUnfolded += countCache(unfolded, unfoldedGroups);
 	}
 	fin.close();
 
-	cout << "Sum: " << sumArrangements << "\n";
+	cout << "Sum1: " << sumArrangements << "\n";
+	cout << "Sum2: " << sumUnfolded << "\n";
 }
