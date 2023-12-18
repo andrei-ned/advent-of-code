df89c74e93d63202134fe83e5c3546752b50aa20
andrei-ned <nedelcuandrei64@gmail.com>
2023-12-18 15:22:34 +0200

    day 18 part 2

--- AoC Day 18/Main.cpp
+++ AoC Day 18/Main.cpp
@@ -14,15 +14,14 @@ using namespace std;
 struct Tile
 {
 	char symbol = '.';
-	string color = "(#000000)";
+
 };
 
 Tile tileMap[N][N];
 
-void dig(int x, int y, string color)
+void dig(int x, int y)
 {
 	tileMap[y][x].symbol = '#';
-	tileMap[y][x].color = color;
 }
 
 void printMap()
@@ -79,7 +78,7 @@ void countArea()
 	cout << "Area is " << sum << "\n";
 }
 
-int main()
+void solvePart1()
 {
 	ifstream fin("input.txt");
 
@@ -87,13 +86,22 @@ int main()
 	char direction;
 	int len;
 
-	int x = N/2;
-	int y = N/2;
-	dig(x, y, "(#000000)");
+	int x = N / 2;
+	int y = N / 2;
+	dig(x, y);
 	while (fin >> direction >> len >> color)
 	{
 		int xDir = 0;
 		int yDir = 0;
+
+		//color.pop_back();
+		//char instruction = color.back();
+		//color.pop_back();
+		////color.erase(0, 2);
+		//color[0] = '0';
+		//color[1] = 'x';
+		//len = stoul(color, nullptr, 16);
+
 		switch (direction)
 		{
 		case 'U':
@@ -113,12 +121,173 @@ int main()
 		{
 			x += xDir;
 			y += yDir;
-			dig(x, y, color);
+			dig(x, y);
 		}
 	}
 	fin.close();
 
 	floodFill();
 	countArea();
-	printMap();
 }
+
+struct Instruction
+{
+	int64_t x, y;
+	uint64_t len;
+	char direction;
+};
+
+void insertIfMissing(vector<int64_t>& vec, int64_t toInsert)
+{
+	if (vec[0] > toInsert)
+	{
+		vec.insert(vec.begin(), toInsert);
+		return;
+	}
+	if (vec[0] == toInsert)
+		return;
+	for (int i = 1; i < vec.size(); i++)
+	{
+		if (vec[i] == toInsert)
+		{
+			return;
+		}
+		if (toInsert > vec[i-1] && toInsert < vec[i])
+		{
+			vec.insert(vec.begin() + i, toInsert);
+			return;
+		}
+	}
+	vec.push_back(toInsert);
+}
+
+struct Wall
+{
+	int64_t from, to;
+	int64_t other;
+};
+
+void solvePart2()
+{
+	ifstream fin("input.txt");
+
+	string color;
+	char direction;
+	uint64_t len;
+
+	int64_t x = 0;
+	int64_t y = 0;
+	
+	vector<int64_t> xPositions{ 0 };
+	vector<int64_t> yPositions{ 0 };
+	vector<Wall> verticalWalls;
+	vector<Wall> horizontalWalls;
+
+	int64_t sumArea = 0;
+	int64_t perimeter = 0;
+
+	while (fin >> direction >> len >> color)
+	{
+		int xDir = 0;
+		int yDir = 0;
+
+		color.pop_back();
+		char instruction = color.back();
+		color.pop_back();
+		color[0] = '0';
+		color[1] = 'x';
+		len = stoul(color, nullptr, 16);
+		perimeter += len;
+
+		switch (instruction)
+		{
+		case '3'://up
+			yDir = -1;
+			break;
+		case '1'://down
+			yDir = 1;
+			break;
+		case '2'://left
+			xDir = -1;
+			break;
+		case '0'://right
+			xDir = 1;
+			break;
+		}
+		int64_t xPrev = x;
+		int64_t yPrev = y;
+		x += len * xDir;
+		y += len * yDir;
+		sumArea += (xPrev * y - yPrev * x);
+		//Wall w;
+		//if (xPrev - x != 0)
+		//{
+		//	w.from = min(xPrev, x);
+		//	w.to   = max(xPrev, x);
+		//	w.other = y;
+		//	horizontalWalls.push_back(w);
+		//}
+		//else if (yPrev - y != 0)
+		//{
+		//	w.from = min(yPrev, y);
+		//	w.to   = max(yPrev, y);
+		//	w.other = x;
+		//	verticalWalls.push_back(w);
+		//}
+		//insertIfMissing(xPositions, x);
+		//insertIfMissing(yPositions, y);
+		cout << "(" << x << "," << y << ")\n";
+
+	}
+	fin.close();
+
+	perimeter /= 2;
+	perimeter++;
+	sumArea /= 2;
+	sumArea += perimeter;
+	cout << "Total area is " << sumArea << "\n";
+	return;
+
+	assert(x == 0);
+	assert(y == 0);
+
+	std::sort(verticalWalls.begin(), verticalWalls.end(), [](Wall& a, Wall& b) {return a.other < b.other;});
+
+	
+	for (int i = 1; i < yPositions.size(); i++)
+	{
+		int wallCount = 0;
+		for (int j = 1; j < xPositions.size(); j++)
+		{
+			int64_t yLen = yPositions[i] - yPositions[i - 1];
+			int64_t xLen = xPositions[j] - xPositions[j - 1];
+			int64_t area = xLen * yLen;
+			//cout << "Area at " << j << "," << i << " is " << area;
+			int yPos = yPositions[i - 1];
+			int wallMin = verticalWalls[j-1].from;
+			int wallMax = verticalWalls[j-1].to;
+
+			if (yPos >= wallMin && yPos < wallMax)
+			{
+				//cout << " (found wall " << j - 1 << ")";
+				wallCount++;
+			}
+			if (wallCount % 2 == 1) // is in dig area
+			{
+				//cout << "!!!";
+				sumArea += area;
+			}
+			//cout << "\n";
+		}
+	}
+
+	cout << "Total area is " << sumArea << "\n";
+}
+
+int main()
+{
+	//solvePart1();
+	solvePart2();
+}
+
+// 139896139742920 is too high
