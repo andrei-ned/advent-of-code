32dcefe19f0a4ec27dcb176dd3e4d628ef695f1c
andrei-ned <nedelcuandrei64@gmail.com>
2024-12-16 23:55:11 +0200

    day 16 part 2

--- 2024/Day 16/solution.cpp
+++ 2024/Day 16/solution.cpp
@@ -8,24 +8,15 @@
 using namespace std;
 
 vector<string> maze;
-int visited[200][200];
-
-void mark_visited(int x, int y, int dir_idx)
-{
-    int mask = 1 << dir_idx;
-    visited[y][x] |= mask;
-}
-
-bool is_visited(int x, int y, int dir_idx)
-{
-    int mask = 1 << dir_idx;
-    int at_pos = visited[y][x];
-    return (at_pos & mask) != 0;
-}
+int scores[200][200][4]; // [y][x][dir_idx]
+int directions_x[] = {1,0,-1,0};
+int directions_y[] = {0,1,0,-1};
+int turn_count[] = {0,1,2,1};
 
 struct exploration
 {
     int x, y, dir_idx, score;
+    vector<pair<int,int>> path;
 };
 
 struct compare {
@@ -35,12 +26,6 @@ struct compare {
     }
 };
 
-int directions_x[] = {1,0,-1,0};
-int directions_y[] = {0,1,0,-1};
-char directions_char[] = {'>','v','<','^'};
-int turn_count[] = {0,1,2,1};
-int min_score = INT_MAX;
-
 void print_maze()
 {
     for (int  y = 0; y < maze.size(); y++)
@@ -49,7 +34,9 @@ void print_maze()
     }
 }
 
-int explore_with_priority(int start_x, int start_y)
+int best_score = INT_MAX;
+set<pair<int,int>> squares_in_best_paths;
+void explore_with_priority(int start_x, int start_y)
 {
     priority_queue<exploration, vector<exploration>, compare> pq;
     pq.push({start_x, start_y, 0, 0});
@@ -57,55 +44,51 @@ int explore_with_priority(int start_x, int start_y)
     {
         exploration e = pq.top();
         pq.pop();
-        if (maze[e.y][e.x] == 'E')
+        if (maze[e.y][e.x] == 'E' && e.score <= best_score)
         {
-            return e.score;
+            best_score = e.score;
+            for (int i = 0; i < e.path.size(); i++)
+                squares_in_best_paths.insert(e.path[i]);
+            squares_in_best_paths.insert(make_pair(e.x,e.y));
         }
-        if (is_visited(e.x,e.y,e.dir_idx))
-            continue;
-        mark_visited(e.x,e.y,e.dir_idx);
-        if (maze[e.y][e.x] == '.')
-            maze[e.y][e.x] = directions_char[e.dir_idx];
 
         for (int i = 0; i < 4; i++)
         {
+            if (i == 2) continue;
             int new_dir_idx = (e.dir_idx + i) % 4;
             int x_dir = directions_x[new_dir_idx];
             int y_dir = directions_y[new_dir_idx];
             int new_x = e.x + x_dir;
             int new_y = e.y + y_dir;
 
-            // Check bounds
-            if (new_y < 0 || new_y >= maze.size())
-                continue;
-            if (new_x < 0 || new_x >= maze[0].size())
-                continue;
-            // Avoid walls and visited spaces
-            if (maze[new_y][new_x] != '#' && !is_visited(new_x,new_y,new_dir_idx))
-                pq.push({new_x, new_y, new_dir_idx, e.score + 1 + 1000 * turn_count[i]});
+            int new_score = e.score + 1 + 1000 * turn_count[i];
+            
+            if (maze[new_y][new_x] != '#' && new_score <= scores[new_y][new_x][new_dir_idx]) // Avoid walls and squares with worse scores
+            {
+                vector<pair<int,int>> path {e.path};
+                path.push_back(make_pair(e.x,e.y));
+                pq.push({new_x, new_y, new_dir_idx, new_score, std::move(path)});
+                scores[new_y][new_x][new_dir_idx] = new_score;
+            }
         }
     }
-    return -1;
 }
 
 int main()
 {
+    fill_n(&scores[0][0][0], 200 * 200 * 4, INT_MAX);
+
     ifstream fin("input.txt");
     string line;
     while (fin >> line)
         maze.push_back(line);
 
-    int score = -1;
     for (int  y = 0; y < maze.size(); y++)
-    {
         for (int x = 0; x < maze[y].size(); x++)
-        {
             if (maze[y][x] == 'S')
-            {
-                score = explore_with_priority(x,y);
-            }
-        }
-    }
-    print_maze();
-    cout << "Min score: " << score << "\n";
+                explore_with_priority(x,y);
+
+    //print_maze();
+    cout << "Min score: " << best_score << "\n";
+    cout << "Unique squares in best paths: " << squares_in_best_paths.size() << "\n";
 }
