9a7f285fcfce0ff94833660a096c4a3625801107
andrei-ned <nedelcuandrei64@gmail.com>
2024-12-17 16:57:23 +0200

    day 17 part 1

---
+++ 2024/Day 17/Day17/Program.cs
@@ -0,0 +1,94 @@
+﻿Computer3Bit computer = new Computer3Bit();
+
+// Read input
+using (var fileStream = File.OpenRead("input.txt"))
+    using (var streamReader = new StreamReader(fileStream))
+    {
+        string line;
+        for (int i = 0; i < 3; i++)
+        {
+            line = streamReader.ReadLine();
+            computer.registers[i] = uint.Parse(line.Split(" ")[2]);
+        }
+        line = streamReader.ReadLine(); // empy line
+        line = streamReader.ReadLine();
+        Console.WriteLine(line);
+        var commaSplit = line.Split(",");
+        computer.instructions = new byte[commaSplit.Length];
+        computer.instructions[0] = byte.Parse(commaSplit[0].Split(" ")[1]);
+        for (int i = 1; i < commaSplit.Length; i++)
+        {
+            computer.instructions[i] = byte.Parse(commaSplit[i]);
+        }
+        
+    }
+
+while (computer.Step());
+Console.WriteLine(computer.output);
+
+class Computer3Bit
+{
+    public ulong[] registers = new ulong[3];
+    public ulong a_reg { get { return registers[0];} set {registers[0] = value;}}
+    public ulong b_reg { get { return registers[1];} set {registers[1] = value;}}
+    public ulong c_reg { get { return registers[2];} set {registers[2] = value;}}
+    public byte[] instructions;
+    public string output = "";
+    private uint instruction_ptr;
+    // Returns false if halting
+    public bool Step()
+    {
+        if (instruction_ptr >= instructions.Length)
+            return false;
+        byte instruction = instructions[instruction_ptr];
+        byte op = instructions[instruction_ptr + 1];
+        switch (instruction)
+        {
+            case 0: // adv
+                DivInstruction(0);
+            break;
+            case 1: // bxl
+                b_reg = b_reg ^ op;
+            break;
+            case 2: // bst
+                b_reg = GetComboOperand(op) % 8;
+            break;
+            case 3: // jnz
+                if (a_reg != 0)
+                {
+                    instruction_ptr = op;
+                    return true; // we're jumping, so don't increment instruction ptr
+                }
+            break;
+            case 4: // bxc
+                b_reg = b_reg ^ c_reg;
+            break;
+            case 5: // out
+                if (output.Length > 0) output += ",";
+                output += $"{GetComboOperand(op) % 8}";
+            break;
+            case 6: // bdv
+                DivInstruction(1);
+            break;
+            case 7: // cdv
+                DivInstruction(2);
+            break;
+        }
+        instruction_ptr += 2;
+        return true;
+
+        void DivInstruction(int target_reg_idx)
+        {
+            int combo = (int)GetComboOperand(op);
+            registers[target_reg_idx] = a_reg >> combo;
+        }
+    }
+
+    private ulong GetComboOperand(byte op)
+    {
+        if (op <= 3)
+            return op;
+        return registers[op - 4];
+    }
+}
+
