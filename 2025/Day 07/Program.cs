fe63285e184c5954b3937937975896663257940d
andrei-ned <nedelcuandrei64@gmail.com>
2025-12-07 17:37:25 +0200

    2025 day 7 part 1

---
+++ 2025/Day 07/Program.cs
@@ -0,0 +1,29 @@
+﻿long noOfSplits = 0;
+
+using (var fileStream = File.OpenRead("input.txt"))
+using (var streamReader = new StreamReader(fileStream))
+{
+    string line = streamReader.ReadLine();
+    HashSet<int> beams = [line.IndexOf('S')];
+    while (!streamReader.EndOfStream)
+    {
+        line = streamReader.ReadLine();
+        //Console.WriteLine($"{line}\nbeams: {string.Join(",",beams)}");
+        for (int i = 0; i < line.Length; i++)
+        {
+            if (line[i] == '^')
+            {
+                if (beams.Contains(i))
+                {
+                    noOfSplits++;
+                    beams.Add(i-1);
+                    beams.Add(i+1);
+                    beams.Remove(i);
+                }
+            }
+        }
+    }
+}
+
+Console.WriteLine($"Total beam splits: {noOfSplits}");
+
