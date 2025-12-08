b0b89d155dd0b48955c76d378d42ac2f9c3a945f
andrei-ned <nedelcuandrei64@gmail.com>
2025-12-08 21:46:29 +0200

    2025 day 8 part 2

--- 2025/Day 08/Program.cs
+++ 2025/Day 08/Program.cs
@@ -33,19 +33,19 @@ combos.Sort((a,b) =>
 HashSet<(int,int)> connections = new HashSet<(int, int)>();
 for (int i = 0; i < NUM_CONNECTIONS; i++) // n shortest connections...
 {
-    Console.WriteLine($"{boxes[combos[i].Item1]} - {boxes[combos[i].Item2]}");
+    //Console.WriteLine($"{boxes[combos[i].Item1]} - {boxes[combos[i].Item2]}");
     connections.Add(combos[i]);
 }
 
-HashSet<int> visited = new HashSet<int>();
+HashSet<int> visitedPartOne = new HashSet<int>();
 
-int Visit(int boxIndex)
+int Visit(int boxIndex, HashSet<int> visited)
 {
     if (visited.Contains(boxIndex))
     {
         return 0;
     }
-    Console.Write(boxes[boxIndex]);
+    //Console.Write(boxes[boxIndex]);
     int visitCount = 1;
     visited.Add(boxIndex);
     for (int i = 0; i < boxIndex; i++)
@@ -53,7 +53,7 @@ int Visit(int boxIndex)
         (int,int) combo = (i,boxIndex);
         if (connections.Contains(combo))
         {
-            visitCount += Visit(i);
+            visitCount += Visit(i, visited);
         }
     }
     for (int i = boxIndex + 1; i < boxes.Count; i++)
@@ -61,7 +61,7 @@ int Visit(int boxIndex)
         (int,int) combo = (boxIndex,i);
         if (connections.Contains(combo))
         {
-            visitCount += Visit(i);
+            visitCount += Visit(i, visited);
         }
     }
     return visitCount;
@@ -70,10 +70,10 @@ int Visit(int boxIndex)
 List<int> circuitLengths = new List<int>();
 for (int i = 0; i < boxes.Count; i++)
 {
-    var c = Visit(i);
+    var c = Visit(i, visitedPartOne);
     if (c > 0)
     {
-        Console.WriteLine($"\ncircuit was len {c}");
+        //Console.WriteLine($"\ncircuit was len {c}");
         circuitLengths.Add(c);
     }
 }
@@ -85,3 +85,26 @@ for (int i = 1; i <= NUM_CIRCUITS; i++)
     product *= circuitLengths[circuitLengths.Count - i];
 }
 Console.WriteLine($"Product of sizes of {NUM_CIRCUITS} largest circuits = {product}");
+
+// --- Part 2 ---
+int idx = 0;
+HashSet<int> visitedPartTwo = new HashSet<int>();
+while (true)
+{
+    var combo = combos[idx];
+
+    visitedPartTwo.Add(combo.Item1);
+    visitedPartTwo.Add(combo.Item2);
+
+    //Console.WriteLine($"{idx} connections and {visitedPartTwo.Count} boxes visited");
+
+    if (visitedPartTwo.Count == boxes.Count)
+    {
+        Console.WriteLine($"Last two junction boxes: {boxes[combo.Item1]} - {boxes[combo.Item2]}");
+        long answer = (long)boxes[combo.Item1].X * (long)boxes[combo.Item2].X;
+        Console.WriteLine($"Their x coords multiply to {answer}"); 
+        break;
+    }
+
+    idx++;
+}
